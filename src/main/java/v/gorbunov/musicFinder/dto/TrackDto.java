3b011c74ed8c73c3d9e7851fe06fbba3e712afa1
Владислав Горбунов <vladgorr1262@gmail.com>
2023-09-27 18:27:48 +0300

    providerName changed to providerCode

--- src/main/java/v/gorbunov/musicFinder/dto/TrackDto.java
+++ src/main/java/v/gorbunov/musicFinder/dto/TrackDto.java
@@ -8,6 +8,6 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class TrackDto {
-    private String providerName;
+    private String providerCode;
     private String trackUrl;
 }
